2d60f38c9f4e3e18afaffd5534342d101ccd72b2
Hurkan KALAN <hurkankalan@Air-de-Hurkan.lan>
2023-03-25 12:30:35 +0100

    creating files for the matchers chapter

---
+++ course2/matchers.spec.js
@@ -0,0 +1,11 @@
+/*
+TYPES
+Pour installer les types Typescript de Jest dans nos projets :
+--> npm i  @types/jest
+VS Code va ainsi pouvoir automatiquement récupérer les types et les arguments des méthodes et fonctions et utiliser l’autocomplétion !
+*/
+
+/*
+MATCHERS
+La méthode expect() permet d’utiliser des méthodes appelés matchers permettant de vérifier si des valeurs respectent des conditions.
+*/
