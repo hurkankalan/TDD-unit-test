bde73116f6c8366eb164b133cf7df0f2bcb90d44
Hurkan KALAN <hurkankalan@MacBook-Air-de-Hurkan.local>
2023-03-30 11:50:09 +0200

    initialization and configuration  typescript

---
+++ build/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const description = "Welcome to test repository ! We will learn application testing with Jest library together !";
+console.log(description);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const description =
+  "Welcome to test repository ! We will learn application testing with Jest library together !";
+
+console.log(description);
