3196e065e8bad3c5833c29cae0a192c3aa642945
Hurkan KALAN <hurkankalan@MacBook-Air-de-Hurkan.local>
2023-04-04 17:24:26 +0200

    finalization introduction to TDD and testing with Jest library

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 const description =
-  "Welcome to test repository ! We will learn application testing with Jest library together !";
+  "Welcome to test repository ! We will learn application testing with Jest library together ! To start, choose your course and change the value of the test property by adding the file name after the word 'jest' so that only the file you have chosen is tested  ";
 
 console.log(description);
